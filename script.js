f61ebda9910408b6d17e790d11755ba31c5c1bb4
n4f1hz4in <93868198+n4f1hz4in@users.noreply.github.com>
2023-04-19 00:53:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const minutes = document.querySelector("#minutes");
 const seconds = document.querySelector("#seconds");
 
 // Set the target date here
-const targetDate = new Date("2023-04-19T00:30:00");
+const targetDate = new Date("2023-04-19T04:30:00");
 
 function updateTime() {
   const now = new Date();
